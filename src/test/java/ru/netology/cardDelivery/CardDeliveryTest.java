d94b888c7a8a7237a8dcffb0f04612348dacbb0a
pavel shuvalov <pmshuv975@gmail.com>
2023-05-21 15:31:00 +0300

    tests for first task

--- src/test/java/ru/netology/cardDelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/cardDelivery/CardDeliveryTest.java
@@ -2,6 +2,7 @@ package ru.netology.cardDelivery;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
@@ -16,8 +17,8 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.*;
 
 class CardDeliveryTest {
-    public static String setLocalDate(int day) {
-        String date = LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    public static String setLocalDate(int addDays) {
+        String date = LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         return date;
     }
 
@@ -31,19 +32,8 @@ class CardDeliveryTest {
         $("[data-test-id=phone] .input__control").setValue("+72557778899");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-//      $(".button__content").click();
         $("[data-test-id=notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
         $("[data-test-id=notification] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + setLocalDate(3)));
 
     }
-
-//    @Test
-//    void shouldTestForDay3() {
-
-
-//        $("[data-test-id=notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
-//        $("[data-test-id=notification] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + setLocalDate(3)));
-//    }
-
-
 }
